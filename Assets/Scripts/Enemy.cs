1c2735dc29676497e7afc59f76135913d9d4cc40
hussar1201 <84184553+hussar1201@users.noreply.github.com>
2021-06-30 17:38:17 +0900

    20210630_1700
    20210630_1700

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,8 +6,6 @@ using UnityEngine.AI;
 public class Enemy : MonoBehaviour
 {
     public Enemy_Nav e_Nav;
-
-
     private PlayerController pc;
     private Rigidbody rb;
     private float speed;
@@ -18,7 +16,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        speed = 5f;     
+        speed = e_Nav.speed*0.9f;     
        
     }
 
@@ -27,7 +25,7 @@ public class Enemy : MonoBehaviour
     {
         if (pos_Nav_before == e_Nav.transform.position) return;
         transform.LookAt(e_Nav.transform);
-        Vector3 heading = e_Nav.transform.position - transform.position;
+        //Vector3 heading = e_Nav.transform.position - transform.position;
         //transform.position += heading.normalized * speed * Time.deltaTime;
         transform.position += transform.forward * speed * Time.deltaTime;
         pos_Nav_before = e_Nav.transform.position;
