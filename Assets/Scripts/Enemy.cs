13bc0a6d0bb94ae5611c6f8173ac293b55e95ccd
hussar1201 <84184553+hussar1201@users.noreply.github.com>
2021-06-30 16:03:22 +0900

    20210630_1600
    20210630_1600

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour
+{
+    public Enemy_Nav e_Nav;
+
+
+    private PlayerController pc;
+    private Rigidbody rb;
+    private float speed;
+    private Vector3 pos_Nav_before;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed = 5f;     
+       
+    }
+
+
+    private void Update()
+    {
+        if (pos_Nav_before == e_Nav.transform.position) return;
+        transform.LookAt(e_Nav.transform);
+        Vector3 heading = e_Nav.transform.position - transform.position;
+        //transform.position += heading.normalized * speed * Time.deltaTime;
+        transform.position += transform.forward * speed * Time.deltaTime;
+        pos_Nav_before = e_Nav.transform.position;
+    }
+
+
+
+}
--- Assets/Scripts/Enemy_Nav.cs
+++ Assets/Scripts/Enemy_Nav.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy_Nav : MonoBehaviour
 {
     private NavMeshAgent pathFinder;
     private PlayerController pc;
 
+    public float speed {
 
+        get;
+        private set;
+    }
     // Start is called before the first frame update
     void Start()
     {
         pathFinder = GetComponent<NavMeshAgent>();
         pc = FindObjectOfType<PlayerController>();
-        pathFinder.speed = 3f;
+        speed = pathFinder.speed;
 
         StartCoroutine("UpdatePath");
     }
@@ -25,14 +29,18 @@ public class Enemy : MonoBehaviour
                     
     }
 
-
-
     private IEnumerator UpdatePath()
     {
         pathFinder.isStopped = false;
+        Debug.Log("TRACKING");
+                
         pathFinder.SetDestination(pc.transform.position);
-        yield return new WaitForSeconds(.3f);
+                
+        yield return new WaitForSeconds(.1f);
     }
+
+
+
 
 
 
