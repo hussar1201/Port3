b81daae30f78c6eda93bd46e348cbee1c6cfbadf
hussar1201 <84184553+hussar1201@users.noreply.github.com>
2021-07-01 09:47:36 +0900

    20210701_1000
    20210701_1000

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -15,7 +15,7 @@ public class Enemy : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        speed = e_Nav.speed * 0.92f;
+        speed = e_Nav.speed * 0.95f;
 
     }
 
@@ -26,17 +26,17 @@ public class Enemy : MonoBehaviour
         if (e_Nav.stopped == false)
         {
 
-
             Vector3 heading = e_Nav.transform.position - transform.position;
 
             rb.MovePosition(transform.position + (heading * speed * Time.deltaTime));
-            //rb.rotation = Quaternion.FromToRotation( e_Nav.transform.position, transform.position);
 
-            transform.LookAt(e_Nav.transform);
+            //transform.position += Vector3.forward * speed * Time.deltaTime;
 
-            //transform.position += heading.normalized * speed * Time.deltaTime;
-            //transform.position += transform.forward * speed * Time.deltaTime;
-            //pos_Nav_before = e_Nav.transform.position;
+
+            transform.LookAt(new Vector3(e_Nav.transform.position.x, transform.position.y, e_Nav.transform.position.z));
+
+            
+
         }
         else if (e_Nav.stopped == true)
         {
@@ -46,15 +46,13 @@ public class Enemy : MonoBehaviour
         }
 
 
+
+
+
     }
 
 
-    private IEnumerator UpdatePath()
-    {
-
-
-        yield return new WaitForSeconds(.1f);
-    }
+    
 
 
 
--- Assets/Scripts/Enemy_Nav.cs
+++ Assets/Scripts/Enemy_Nav.cs
@@ -7,6 +7,7 @@ public class Enemy_Nav : MonoBehaviour
 {
     private NavMeshAgent pathFinder;
     private PlayerController pc;
+    private Transform transform_parent;
 
     public float speed {
 
@@ -19,6 +20,8 @@ public class Enemy_Nav : MonoBehaviour
     void Start()
     {
         pathFinder = GetComponent<NavMeshAgent>();
+        transform_parent = GetComponentInParent<Transform>();
+        Debug.Log(transform_parent);
         pc = FindObjectOfType<PlayerController>();
         speed = pathFinder.speed;       
     }
@@ -27,9 +30,15 @@ public class Enemy_Nav : MonoBehaviour
     void Update()
     {
         pathFinder.isStopped = false;
+
+
         if (pathFinder.velocity == Vector3.zero) { stopped = true;}
         else stopped = false;
-        pathFinder.SetDestination(pc.transform.position); 
+      
+        pathFinder.SetDestination(pc.transform.position);
+        transform_parent.LookAt(pc.transform);      
+
+        
     }
 
 
